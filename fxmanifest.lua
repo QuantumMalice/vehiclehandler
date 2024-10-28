4cbde1edb92be029b718a3e7e9a58967c08d8c77
QuantumMalice <QuantumMalice@users.noreply.github.com>
2024-10-28 22:23:28 +0000

    chore: bump manifest version to v1.3.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54       'yes'
 name        'vehiclehandler'
 description 'Collision/damage handling for FiveM.'
 author      'QuantumMalice'
-version '1.3.2'
+version '1.3.3'
 
 files {
     'data/progress.lua',
