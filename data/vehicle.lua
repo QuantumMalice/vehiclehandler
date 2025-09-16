0e45e3fedde373d0c546f79f2a6550ce59472c48
Abraham Torres Torres <abraham.torres6647@alumnos.udg.mx>
2025-09-16 04:54:17 -0600

    Reformatted regulated exclusions in vehicle.lua

--- data/vehicle.lua
+++ data/vehicle.lua
@@ -60,6 +60,7 @@ return {
     regulated_exclusions = {    -- Prevent rotation controls for these specific models, even if their class is regulated
         [`deluxo`] = true,
         [`scramjet`] = true,
+        [`vigilante`] = true,
     },
     backengine = {
         [`ninef`] = true,
